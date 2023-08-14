cb49b57ca523b995c3031c803263ed38c9f967b4
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-13 21:56:36 -0700

    print five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main header
+ *
+ * Return - return o
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
