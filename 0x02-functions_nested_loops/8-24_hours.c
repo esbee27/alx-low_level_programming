98edf73697375a9901720f151cfe39a81801a210
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-17 03:17:35 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,17 @@
 
 void jack_bauer(void)
 {
-	int i;
-	for (i = 00 : 00 ; i >= 23 : 59 ; i++)
-		return (i);
+	int i, j;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+	}
 }
