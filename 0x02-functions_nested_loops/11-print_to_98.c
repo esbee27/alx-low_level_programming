92064227a59fc0cdd4c23d13a26e32ad4eff3ad7
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-17 10:35:08 -0700

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,14 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n ; i <= 98 ; i++)
 	{
-		if (i >= 0 && i < 98)
-			printf("%d, ", i);
-		else if (i == 98)
-			printf("%d", i);
+		for (i = n ; i <= 98 ; i++)
+		{
+			if (i >= 0 && i < 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d", i);
+		}
 		printf("\n");
 	}
 	for (i = n ; i >= 98 ; i--)
@@ -41,6 +43,6 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		else if (i == 98)
 			printf("%d", i);
-		printf("\n");
 	}
+	printf("\n");
 }
