1b4e62125767d3063a35862d977ad323f3f7ea1d
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-20 07:40:29 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - more numbers
+ * Return: returns o
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 15; i++)
+		_putchar(i + '0');
+	{
+		for (j = 0; j <= 10; j++)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
