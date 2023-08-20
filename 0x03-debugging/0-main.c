06171810c33afbc2b7bcb5bc1ecfa4a13f1a013d
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-19 17:43:47 -0700

    summit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* positive_or_negative - main function
 * main - tests function that prints if integer is positive or negative
+* positive_or_negative - main function
 * Return: 0
 */
 
@@ -12,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
