c2a2bf48635e3f35883dac51b2a4a11629efaa7c
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-20 05:48:26 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - main function
+ * Return: returns numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i != 2 && i != 9)
+			_putchar(i + '0');
+	_putchar('\n');
+}
