705e2fa86c51deb85e5de74b2eaef07f6eb0f1f6
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-20 18:44:47 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,12 +14,15 @@ void print_diagonal(int n)
 	{
 		int i;
 
-		for (i = 1; i <= n; i++)
-			if (i < n)
-				_putchar('\n');
-			else
-				_putchar('\\');
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j =  0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
 
