789146d854bf8a9dd6c38c3e4d1c1b75a77937c6
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-22 09:32:58 -0700

    commmit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half string
+ * Return: returns 0
+ * @str: a value
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int count = 0;
+	int n;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
