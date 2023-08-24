f4752e56a4b159ed9bcb17882241fe0b3edf5332
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-24 07:57:31 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - main function
+ * @str: a value
+ * Return: returns 0
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
