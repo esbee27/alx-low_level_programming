364a74b7d64df40405793d13d8cb3668fcc8a09f
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-24 03:41:28 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates
+ * Return: returns 0
+ * @dest: a value
+ * @src: a value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		dest++;
+	for (i = 0; src[i] != '\0'; i++)
+		src++;
+	for (i = 0; i <= srclen; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
