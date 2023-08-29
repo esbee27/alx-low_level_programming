bc810466d9e8321c6cd102e6f6f37d7d1081c2cb
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-29 09:13:11 -0700

    haystack

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,7 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #define MAIN_H
 #endif
