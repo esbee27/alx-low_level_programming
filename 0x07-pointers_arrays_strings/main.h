092a435f6e6db65e20517701d3e8133d5a3b47c8
Blessing Ogor <ogorblessing96@gmail.com>
2023-08-29 08:14:26 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #define MAIN_H
 #endif
