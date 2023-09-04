8483bb14d6ebbf4353c64236f3f13d1ed7e83138
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-04 07:04:11 -0700

    static library

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -7,22 +7,4 @@
  * Return: returns a value
  */
 
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int i, n, check, value;
-
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			check = 0;
-
-			for (n = 0; accept[n] != '\0'; n++)
-			{
-				if (accept[n] == s[i])
-				{
-					value++;
-					check = 1;
-				}
-			}
-		}
-		if (check == 0)
-}
+unsigned int _strspn(char *s, char *accept);
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void *_memset(void);
-char *_memcpy(void);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
