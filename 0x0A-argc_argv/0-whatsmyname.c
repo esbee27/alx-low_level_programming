ce01ca68f4690efcedc8d23d734aea612072c302
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-05 06:25:57 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: a variable
+ * @argv: an array
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
