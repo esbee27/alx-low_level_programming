e483651a6813e7670652d9ad65f70f4004dc2304
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-05 06:36:38 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints no of arg
+ * @argv: a value
+ * @argc: an input
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+		return (0);
+}
