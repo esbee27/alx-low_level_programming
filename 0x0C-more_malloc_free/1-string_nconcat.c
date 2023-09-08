74ad9599fa91d9e38e5062bd2f24b2581065abb0
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-08 04:38:44 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			output[i] = s1[i];
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 			output[s1len + i] = s2[i];
 		output[s1len + i] = '\0';
 	}
