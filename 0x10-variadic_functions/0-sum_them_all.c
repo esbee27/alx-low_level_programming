7d53a84a8d30c348ee85a9302787f3d164141090
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-15 02:29:24 -0700

    add them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,18 @@
  */
 
 int sum_them_all(const unsigned int n, ...)
-{	unsigned int i;
-	
+{
+	unsigned int i;
 	int sum = 0;
 
 	va_list arg;
-	va_start(arg, n);
 
-	if (n == 0)
-		return (0);
+	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
 		sum = sum + va_arg(arg, int);
 	}
-	printf("%d\n", sum);
 	va_end(arg);
 
 	return (sum);
