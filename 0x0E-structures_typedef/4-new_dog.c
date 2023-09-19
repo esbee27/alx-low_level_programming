29e6a00d847b88d963c12a166c7c57758fee2e21
Blessing Ogor <ogorblessing96@gmail.com>
2023-09-19 13:46:05 -0700

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,48 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
  * _strlen - returns the length of string
- * @s: a value
+ * @str: a value
  * Return: returns o
  */
 
-int _strlen(char *s)
+int _strlen(char *str)
 {
-	int i;
+	int len = 0;
 
-	i = 0;
+	while (*str)
+		len++;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	return (len);
 }
 
 /**
- * _strcpy - ,ain function
+ * _strcopy - ,ain function
  * @dest: a value
  * @src: a value
  * Return: returns d
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcopy(char *dest, char *src)
 
 {
-	int len, i;
+	int index = 0;
 
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+	dest[index] = '\0';
 
 	return (dest);
 }
@@ -59,27 +52,33 @@ char *_strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog _t *dog;
-	int len1, len2;
+	dog_t *dog;
 
-	len1 = strlen(name);
-	len2 = strlen(owner);
+	if (name == NULL || age == 0 || owner == NULL)
+		return (NULL);
 
-	dog = malloc(sizeof(dog));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->name = malloc(sizeof(char) * (len2 + 1));
+
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog);
 		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
+
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+
+	return (dog);
 }
