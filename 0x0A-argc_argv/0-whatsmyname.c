deec6b5a28c6e3d5c613e7bb7339cdeeb814d6af
Blessing Ogor <ogorblessing96@gmail.com>
2024-01-12 13:18:10 +0100

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * main - main function
+ * @argv: argument
+ * @argc: argument
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", aorgv[0]);
+
+	return (0);
+}
+
