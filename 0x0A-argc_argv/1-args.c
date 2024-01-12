6208cafd56d8f911da1bad200af86066e1ef7054
Blessing Ogor <ogorblessing96@gmail.com>
2024-01-12 05:04:12 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include "stdio.h"
 
+/**
+ * main - main function
+ * @argc: a value
+ * @argv: a value
+ * Return: returns 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argv;
