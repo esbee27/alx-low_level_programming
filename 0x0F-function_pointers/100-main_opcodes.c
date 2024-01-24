d57f0a7ac97773985d525329f249969e2d3a520e
Blessing Ogor <ogorblessing96@gmail.com>
2024-01-24 16:00:28 +0100

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,7 +18,7 @@ int main (int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[i]);
+	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
@@ -27,14 +27,14 @@ int main (int argc, char *argv[])
 	}
 	arr = (char *)main;
 
-	for (i = 0; i< bytes; i++)
+	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx ", arr[i]);
+		printf("%02hhx", arr[i]);
 	}
 	return (0);
 }
