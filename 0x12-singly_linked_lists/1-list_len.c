d4c5db510d2b8d65e7c622730b032a36fa8613ac
Blessing Ogor <ogorblessing96@gmail.com>
2024-02-17 19:51:04 +0100

    prints length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * list_len - prints number of elements
+ * @h: pointer to structure
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
