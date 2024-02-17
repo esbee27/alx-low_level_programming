f43d247f5a79a5971553235dd334be55d07b54c1
Blessing <ogorblessing96@gmail.com>
2024-02-17 13:24:19 +0100

    Frees a list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - prints a string before main
+ * Return: no return
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
