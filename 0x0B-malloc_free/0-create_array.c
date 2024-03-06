9b63b7c65394d31c8575f4017be2cc9621db3090
Blessing <ogorblessing96@gmail.com>
2024-03-06 10:19:07 +0100

    Corrected file1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,8 +18,8 @@ char *create_array(unsigned int size, char c)
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < size; 1++)
+	for (i = 0; i < size; i++)
 		str[i] = c;
 
-	return (c);
+	return (str);
 }
